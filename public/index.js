cb0d457e1134dd643130086f8a120de252f47e97
Charaka <charaka@basilurtea.com>
2021-09-25 12:52:42 +0530

    Inital commit

---
+++ public/index.js
@@ -0,0 +1,28 @@
+function Spa() {
+  const [user, setUser] = React.useState(null);
+
+  return (
+    <HashRouter>
+      <div>
+        <UserContext.Provider value={{ user, setUser }}>
+          <Route path="/" component={NavBar} />
+          <div className="container" style={{padding: "20px"}}>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/login/" component={Login} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            {/* <Route path="/transactions/" component={Transactions} /> */}
+            <Route path="/balance/" component={Balance} />
+            <Route path="/alldata/" component={AllData} />
+          </div>
+        </UserContext.Provider>
+      </div>
+    </HashRouter>
+  );
+}
+
+ReactDOM.render(
+  <Spa/>,
+  document.getElementById('root')
+);
